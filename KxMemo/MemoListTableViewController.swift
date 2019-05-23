b4fb70a71d203213de28269b9073bf723d52e899
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-05-23 16:46:44 +0900

    3 완성

--- KxMemo/MemoListTableViewController.swift
+++ KxMemo/MemoListTableViewController.swift
@@ -30,10 +30,7 @@ class MemoListTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
